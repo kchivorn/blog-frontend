fa5e6142bc18daca2bb1d392f3e06024339aa1ee
kchivorn <kouch.chivorn@gmail.com>
2021-08-08 20:27:04 +0700

    Add view page for post and render all its comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = PostService.all
   end
+
+  def show
+    @post = PostService.find(params[:id])
+    @comments = PostService.find_comments(params[:id])
+  end
 end
